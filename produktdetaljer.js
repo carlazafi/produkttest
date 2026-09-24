e41d26ffb21f7b458bef41975717803e36c41cbc
Carla Zafirakos <caza1000@stud.ek.dk>
2026-09-24 11:35:36 +0200

    Små rettelser

--- produktdetaljer.js
+++ produktdetaljer.js
@@ -19,7 +19,7 @@ function visData(element) {
     console.log(element);
         produkt.innerHTML = 
         `<a class="card" href=produktdetaljer.html?id=${element.id}>
-        <article >
+        <article class="card">
         <img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp alt="produktbillede" />
             <h2>${element.productdisplayname}</h2>
             <h3>${element.brandname}</h3>
